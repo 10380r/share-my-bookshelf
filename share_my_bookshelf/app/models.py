eb462930abf13ab21370c6f2a3269d21b2e83f48
Tomiyama <10380r@gmail.com>
2020-05-07 22:40:47 +0900

    update: create Book table

--- share_my_bookshelf/app/models.py
+++ share_my_bookshelf/app/models.py
@@ -1,31 +1,39 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class Post(models.Model):
-    username    = models.ForeignKey(User, on_delete=models.CASCADE, \
-                  related_name = 'post_username')
-    post_date   = models.DateTimeField(auto_now_add=True)
-    isbn_code   = models.TextField(max_length=13, default="")
-    author      = models.TextField(default="")
-    title       = models.TextField(default="")
-    review      = models.TextField(max_length=1000, default="")
-    label       = models.CharField(default=None, max_length=15)
-    star        = models.IntegerField(default=0)
-    like        = models.IntegerField(default=0)
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    isbn_code = models.IntegerField(default=0)
+    post_date = models.DateTimeField(auto_now_add=True)
+    review = models.TextField(max_length=1000, default="")
+    label = models.CharField(default=None, max_length=15)
+    star = models.IntegerField(default=0)
+    like = models.IntegerField(default=0)
 
     def __str__(self):
-        return '%s (written by: %s)' %(self.title, self.author)
+        return "%s (Post ID: %s)" % (self.username, self.id)
 
     class Asc_date:
         # - にしておくと昇順で表示
-        ordering = ('-pub_date',)
+        ordering = ("-pub_date",)
+
+
+class Book(models.Model):
+    isbn_code = models.ForeignKey(
+        Post, on_delete=models.CASCADE
+    )
+    author = models.TextField(default="")
+    title = models.TextField(default="")
+
 
 class Friend(models.Model):
-    myself      = models.ForeignKey(User, on_delete=models.CASCADE,\
-                  related_name='friend_myself')
-    friend      = models.ForeignKey(User, on_delete=models.CASCADE)
+    myself = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="friend_myself"
+    )
+    friend = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '%s (%s)' %(self.friend)
-
-        # change tomiyama
+        return "%s (%s)" % (self.friend)
