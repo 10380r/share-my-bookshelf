bad6c01ba829c03d21b393e09645039d92e132f4
Takumi <watanabe@example.com>
2020-05-22 09:35:24 +0900

    編集：LABELのプルダウン表示

--- share_my_bookshelf/app/forms.py
+++ share_my_bookshelf/app/forms.py
@@ -2,7 +2,15 @@ from django import forms
 from .models import Post
 
 class PostCreateForm(forms.ModelForm):
+    CHOICE = [
+        ('1', 'business'),
+        ('2', 'history'),
+        ('3', 'teqcnology'),
+    ]
+    label = forms.fields.ChoiceField(
+        required=True,
+        choices = CHOICE,
+        widget=forms.Select(attrs={'id':'one'}))
     class Meta:
         model = Post
-        fields = ['username', 'isbn_code', 'review',
-        'label', 'star', 'was_read',]
+        fields = ['username', 'isbn_code', 'review', 'star', 'was_read',]
